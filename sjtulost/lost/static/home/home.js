5191cf8d2e021051c28edde736f8666d97a65aad
钩钩么么哒 <a516550257@qq.com>
2016-04-22 18:22:06 +0800

    首页导航样式

--- sjtulost/lost/static/home/home.js
+++ sjtulost/lost/static/home/home.js
@@ -2,10 +2,41 @@
  * Created by gougoumemeda on 16/4/22.
  */
 
-var App = React.createClass({
+
+var Navigation = React.createClass({
     render: function() {
         return (
-            <p>fuck?</p>
+            <div className="navbar navbar-default navbar-fixed-top">
+                <div className="container">
+                    <div className="navbar-header">
+                        <a href="#" className="navbar-brand">SJTU Lost</a>
+                    </div>
+                    <div className="navbar-collapse collapse" id="navbar-main">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <a href = "#">丢失</a>
+                            </li>
+                            <li>
+                                <a href = "#">拾物</a>
+                            </li>
+                            <li>
+                                <a href = "#">排行</a>
+                            </li>
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a href="#">使用Jaccount登录</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+})
+
+var HomePage = React.createClass({
+    render: function() {
+        return (
+            <Navigation />
         )
     }
 })
@@ -13,7 +44,7 @@ var App = React.createClass({
 
 
 React.render(
-    <App />,
+    <HomePage />,
     document.getElementById('content')
 );
 
